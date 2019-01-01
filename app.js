5db661238bcfbf16f6b8118e1073faa4e12b6002
Chris <c.d.harris.dedoc@gmail.com>
2019-01-02 08:23:04 +1100

    updateForDashJS
    Login Functionality with jQuery toggle
    Tabs display function performed by vanilla JS

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function openDash(dashName,elmnt,color) {
+function viewDash(dashName,elmnt,color) {
     var i, dashcontent, dashlinks;
 
     dashcontent = document.getElementsByClassName('dashcontent');
@@ -7,80 +7,10 @@ function openDash(dashName,elmnt,color) {
     }
 
     dashlinks = document.getElementsByClassName('dashlinks');
-    for (i = 0; i < dashlinks.length; i++) {
+    for (i=0; i < dashlinks.length; i++) {
         dashlinks[i].style.backgroundColor = "";
     }
 
     document.getElementById(dashName).style.display = "block";
     elmnt.style.backgroundColor = color;
 }
-
-// // get the element with id defaultOpen and click on it
-// document.getElementById("defaultOpen").click();
-
-function getUsername() {
-  var user;
-  var loginBtn = document.getElementById("loginBtn");
-  var inputValue = prompt("Please Enter UserName:", "Username Here!");
-
-  if (inputValue == null || inputValue == "") {
-    user = "No User Selected!";
-  } else {
-    user = "Welcome Back, " + inputValue + "! " ;
-  }
-  document.getElementById("username").innerHTML = user;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function displayUser() {
-//     document.getElementById('username').innerHTML =
-//         document.getElementById('usernameInfo').value;
-// }
-
-// var username;
-// var submitBtn;
-
-// function getUser() {
-//     username = document.getElementById('usernameInfo');
-//     document.getElementById('username').innerHTML = value;
-
-// }
-
-// function loginBtn() {
-//     submitBtn = documnt.getElementById('userBtn');
-//     submitBtn.addEventListener('click');
-// }
